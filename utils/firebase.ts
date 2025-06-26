17ac6da77146798f872893c722e15c0b0c0f2e9e
Yaniv Briker <brikeryaniv@gmail.com>
2025-06-26 02:12:01 -0700

    begin firebase

---
+++ utils/firebase.ts
@@ -0,0 +1,20 @@
+import { FirebaseOptions, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyD_YP_cl_lI4eCHTWzuN5_Bjiyb_Y4z7TQ",
+  authDomain: "video-sync-10531.firebaseapp.com",
+  projectId: "video-sync-10531",
+  storageBucket: "video-sync-10531.firebasestorage.app",
+  messagingSenderId: "820825199730",
+  appId: "1:820825199730:web:13c7ac7ace788a95cb5eeb",
+  measurementId: "G-78R129K63L",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
