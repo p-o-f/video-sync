36c5c71d2e2248bab461cf2dcbbf11662a1aa7a2
Yaniv Briker <brikeryaniv@gmail.com>
2025-06-26 02:12:01 -0700

    begin firebase

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
 import './App.css';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '@/utils/firebase';
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  }
   return (
     <>
       <h1>yo</h1>
+      <button onClick={handleLogin}>Login</button>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => { console.log("hi"); setCount((count) => count + 1) }}>
           count is {count}
         </button>
       </div>
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    content_security_policy: {
+      extension_pages:
+        "script-src 'self' https://www.google-analytics.com/ https://apis.google.com/; object-src 'self'",
+    },
+  },
 });
